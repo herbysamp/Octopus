3d8e57fb2f497f08b6ca96ba582856e8cfb8c5b9
herbysamp <72114865+herbysamp@users.noreply.github.com>
2021-02-18 12:23:36 +0700

    Create mute.js

---
+++ commands/administrator/mute.js
@@ -0,0 +1,122 @@
+const Discord = require('discord.js');
+
+const config = require('../../configs/config.json');
+
+module.exports = {
+
+    config: {
+
+        name: 'mute',
+
+        description: `mute members who don't adhere to rules`,
+
+        aliases: [""],
+
+        usage: '@user reason',
+
+        accessableby: "MANAGE_ROLES",
+
+    },
+
+    run: async (client, message, args) => {
+
+    if (!message.member.hasPermission("MANAGE_ROLES")) {
+
+      return message.channel.send(
+
+        "You will need the `MANAGE_ROLES` permissions"
+
+      );
+
+    }
+
+    if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+
+      return message.channel.send(" I don't have access Manage_Roles.");
+
+    }
+
+    const user = message.mentions.members.first();
+
+    
+
+    if(!user) {
+
+      return message.channel.send("Please mention the member to who you want to mute")
+
+    }
+
+    
+
+    if(user.id === message.author.id) {
+
+      return message.channel.send("I won't mute you -_-");
+
+    }
+
+    
+
+    
+
+    let reason = args.slice(1).join(" ")
+
+    
+
+    
+
+    if(!reason) {
+
+      return message.channel.send("Please Give the reason to mute the member")
+
+    }
+
+    
+
+  //TIME TO LET MUTED ROLE
+
+    
+
+    let muterole = message.guild.roles.cache.find(x => x.name === "Muted")
+
+    
+
+    
+
+      if(!muterole) {
+
+      return message.channel.send("This server do not have role with name `Muted`")
+
+    }
+
+    
+
+    
+
+   if(user.roles.cache.has(muterole)) {
+
+      return message.channel.send("Given User is already muted")
+
+    }
+
+    
+
+  
+
+    
+
+    
+
+    user.roles.add(muterole)
+
+    
+
+await message.channel.send(`You muted **${message.mentions.users.first().username}** For \`${reason}\``)
+
+    
+
+    user.send(`You are muted in **${message.guild.name}** For \`${reason}\``)
+
+    }
+
+}
+
